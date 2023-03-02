9416939d9050cbe862cc3d27d10b6d953f2d7932
Hrihorii Ilin <devopsick@pm.me>
2023-03-03 02:29:45 +0300

    Master

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@
 import '../styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import Head from "next/head";
+import "bootswatch/dist/sketchy/bootstrap.min.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function MyApp({ Component, pageProps }) {
   return (
